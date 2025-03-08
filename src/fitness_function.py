248942b29269cec17ae7ade949ea9e7ff939d541
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-08 20:58:23 +0300

    Add field function
    Now constructor applies function in string and convert it to sympy
    format

--- src/fitness_function.py
+++ src/fitness_function.py
@@ -2,15 +2,23 @@ import sympy
 
 
 class FitnessFunction:
-    def __init__(self, *, variables: sympy.symbols, coefficients: list[float]):
+    def __init__(self, *, function: str, variables: sympy.symbols):
         """
         Целевая функция имеет стандартизированный вид ДОПИСАТЬ
 
         :param coefficients: Коэффициенты составляющих целевой функции
         """
 
+        self.function = function
         self.variables = variables
-        self.coefficients = coefficients
+
+    @property
+    def function(self):
+        return self._function
+
+    @function.setter
+    def function(self, function: str):
+        self._function = sympy.sympify(function)
 
     @property
     def variables(self):
@@ -24,33 +32,4 @@ class FitnessFunction:
             self._variables = variables
 
     def __str__(self):
-        terms = []  # Список для хранения слагаемых
-
-        num_vars = len(self.variables)
-        coef_index = 0
-
-        # Квадратичные члены (x1**2, x2**2 и т. д.)
-        for i in range(num_vars):
-            terms.append(f"{self.coefficients[coef_index]}*{self.variables[i]}**2")
-            coef_index += 1
-
-        # Попарные произведения (x1*x2, x1*x3 и т. д.)
-        for i in range(num_vars):
-            for j in range(i + 1, num_vars):
-                terms.append(f"{self.coefficients[coef_index]}*{self.variables[i]}*{self.variables[j]}")
-                coef_index += 1
-
-        # Линейные члены (x1, x2 и т. д.)
-        for i in range(num_vars):
-            terms.append(f"{self.coefficients[coef_index]}*{self.variables[i]}")
-            coef_index += 1
-
-        # Формируем строку
-        return f"f(x) = {' + '.join(terms)}"
-
-fitness_function1 = FitnessFunction(variables=sympy.symbols('x1 x2'), coefficients=[2, 2, 2, -4, -6])
-print(fitness_function1)
-
-
-
-
+        return f"f(x) = {self._function}"
