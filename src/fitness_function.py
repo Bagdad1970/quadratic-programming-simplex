f02a27beaf15ad9ec6e6f0bb12d12f150edbc56b
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-12 14:01:10 +0300

    Refactoring: Remove function variables from FitnessFunction

--- src/fitness_function.py
+++ src/fitness_function.py
@@ -2,15 +2,12 @@ import sympy
 
 
 class FitnessFunction:
-    def __init__(self, *, function: str, variables: sympy.symbols):
+    def __init__(self, function: str):
         """
-        Целевая функция имеет стандартизированный вид ДОПИСАТЬ
-
-        :param coefficients: Коэффициенты составляющих целевой функции
+        :param function: Целевая функция
         """
 
         self.function = function
-        self.variables = variables
 
     @property
     def function(self):
@@ -20,16 +17,5 @@ class FitnessFunction:
     def function(self, function: str):
         self._function = sympy.sympify(function)
 
-    @property
-    def variables(self):
-        return self._variables
-
-    @variables.setter
-    def variables(self, variables: sympy.symbols):
-        if isinstance(variables, sympy.core.symbol.Symbol):
-            self._variables = (variables,)
-        else:
-            self._variables = variables
-
-    def __str__(self):
-        return f"f(x) = {self._function}"
+    def __add__(self, other):
+        return self.function + other.function
